d01cf3b415a3b2b40990340c8ff5526b639b29b6
John Dyer <john@krumpt.com>
2010-08-29 23:58:57 -0400

    working on reports page, added button to control view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,18 +1,21 @@
 class ReportsController < ApplicationController
   
+  before_filter :require_doctor
+  
+  
   def index
-    #@patients = Patient.all(:order=>'facility')
-    #@patients_charges = Patient.all(:order=>'facility')
-    #@patients = Patient.facility_like_all(params[:search].to_s.split).ascend_by_room
-
-
     @search=Charge.search(params[:search] || {:recorded=>false})
-    @charges=@search.all
-    respond_to do |format|
-
-      format.html # index.html.erb
-      format.xml  { render :xml => @charges }
-    end
+    @reports=@search.all
+     respond_to do |format|
+        format.html # index.html.erb
+        format.xml  { render :xml => @reports }
+      end
   end
   
+  def show_only_recorded
+    logger.info { "" }
+    logger.info { "@@@ Log: #{params.inspect}" }
+    redirect_to "/reports/"
+    
+  end
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,11 @@
 module ReportsHelper
+  
+  def charge_recorded?(recorded)
+     if !recorded
+       return "no"
+     else
+       return  image_tag("check_mark.png", :border=>0) 
+     end
+   end
+   
 end
