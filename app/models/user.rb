f6e8b2af6c0e881af2d936e17bdff64e2b3a164c
John Dyer <john@krumpt.com>
2010-10-31 12:34:13 -0400

    Associated the Users model w/ charges and patient. Added first_name, last_name, and username to attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
+
+  default_value_for :favorites, "--- []\\n\\n"
+
+  belongs_to :charge
+  belongs_to :patient
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :username, :favorites, :last_name, :first_name, :password, :password_confirmation, :remember_me
 end
