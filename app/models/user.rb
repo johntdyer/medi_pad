a1fc2f2fe44745e7c4b2d3bafb7efc378aada48e
John Dyer <john@krumpt.com>
2010-10-31 22:27:11 -0400

    Added requires validation to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ActiveRecord::Base
 
   default_value_for :favorites, "--- []\\n\\n"
-
+  default_value_for :validated, false
+  default_value_for :is_admin, false
   belongs_to :charge
   belongs_to :patient
 
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  # :token_authenticatable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,:confirmable,:lockable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :username, :favorites, :last_name, :first_name, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :username, :favorites, :last_name, :validated, :first_name, :password, :password_confirmation, :remember_me
 end
