31ae1505f468120fecef0ca1b057dc8b5484e36c
John Dyer <john@krumpt.com>
2010-11-03 20:45:37 -0400

    send_grid setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -6,4 +6,14 @@
 #   :password             =>  'cfpgtestone@gmail.com',  
 #   :authentication       => "plain",  
 #   :enable_starttls_auto => true  
-# }
+# }                                              
+
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => '25',
+  :domain => ENV['SENDGRID_DOMAIN'],
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD']
+}
