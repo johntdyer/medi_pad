90e0584ad37b0d1d00cceae8eb9bb59547da474f
John Dyer <john@krumpt.com>
2010-12-16 18:45:41 -0400

    Added charge_recorded helper

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -7,6 +7,16 @@ module ChargesHelper
 	def lookup_procedure_name(procedure_code)
 	  @procedure =  Procedure.all(:conditions => { :procedure_code => procedure_code })
 	end
+
+  def charge_recorded?(recorded)
+     if !recorded
+       return false
+     else
+       return true
+       image_tag("check_mark.png", :border=>0) 
+     end
+   end
+
 	
 	
 end
