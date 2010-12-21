b15edb6a823b8d85a7228867f39896567dc28f6a
John Dyer <john@krumpt.com>
2010-12-21 07:53:38 -0500

    cleaned up .gitignore file

--- db/schema.rb
+++ db/schema.rb
@@ -67,14 +67,6 @@ ActiveRecord::Schema.define(:version => 20101217183859) do
 
   add_index "doctors", ["perishable_token"], :name => "index_doctors_on_perishable_token"
 
-  create_table "events", :force => true do |t|
-    t.string   "name"
-    t.datetime "start_at"
-    t.datetime "end_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "imports", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
