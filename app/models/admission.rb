8e94ed7ef327326602ba3a95263607033495b7c4
John Dyer <john@krumpt.com>
2011-01-15 23:28:24 -0500

    Added a few named scopes to Admission model ( checked_in, today, yesterday)

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,10 +1,24 @@
 class Admission < ActiveRecord::Base
   
-  
   validates_presence_of :patient_id
-  belongs_to :patient 
-	
-	
-
+  belongs_to :patient
   
+    scope :today, lambda {
+      where(['checkin= ?', DateTime.today])
+    }
+
+    scope :yesterday, lambda {
+      where(['checkin= ?', DateTime.today-1.day])
+    }
+
+    scope :since, lambda{|date|
+      where(['checkin >= ?', date])
+    }
+
+  def self.checked_in 
+    Admission.scoped(
+                  :conditions=>['checkin= ?', Date.today],
+                  :include=>:patient
+                ).map(&:patient).uniq
+  end
 end
